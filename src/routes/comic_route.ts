404c73454835047292e8445502be01180cb90f0c
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-18 10:07:10 -0300

    :recycle: refactor(comic-routes): exchange create new comic route for fetch comics route

--- src/routes/comic_route.ts
+++ src/routes/comic_route.ts
@@ -3,7 +3,7 @@ import ComicController from '../app/controllers/comic_controller';
 
 const comicRouter = Router();
 
-comicRouter.post('/comics', ComicController.addNewComic);
+comicRouter.get('/fetch-comics', ComicController.fetchComics);
 comicRouter.get('/comics', ComicController.getAllComics);
 comicRouter.put('/comic/:comicId', ComicController.updateComicInfo);
 comicRouter.delete('/comic/:comicId', ComicController.deleteComicInfo);
