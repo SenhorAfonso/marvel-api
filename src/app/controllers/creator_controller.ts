92aeb635aaf49983f2d95b318a72e312f80da023
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-21 19:58:40 -0300

    :recycle: refactor(creator-repository): move user response to controller layer

--- src/app/controllers/creator_controller.ts
+++ src/app/controllers/creator_controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import StatusCodes from 'http-status-codes';
 import CreatorService from '../services/creator_service';
 
 class CreatorController {
@@ -7,15 +8,23 @@ class CreatorController {
     req: Request,
     res: Response
   ) {
-    const { success, message, status, result } = await CreatorService.fetchCreators();
-    res.status(status).json({ code: status, success, message, data: { result } });
+    const success: boolean = true;
+    const message: string = 'The creators were registered!';
+    const status: number = StatusCodes.OK;
+
+    const { result } = await CreatorService.fetchCreators();
+    res.status(status).json({ code: status, success, message, data: { result, available: result.length } });
   }
 
   static async getCreators(
     req: Request,
     res: Response
   ) {
-    const { success, message, status, result } = await CreatorService.getCreators(req.body);
+    const success: boolean = true;
+    const message: string = 'All creators were retrieved!';
+    const status: number = StatusCodes.OK;
+
+    const { result } = await CreatorService.getCreators(req.body);
     res.status(status).json({ code: status, success, message, data: { result, available: result.length } });
   }
 
@@ -23,9 +32,13 @@ class CreatorController {
     req: Request,
     res: Response
   ) {
+    const success: boolean = true;
+    const message: string = 'The creator were updated!';
+    const status: number = StatusCodes.OK;
+
     const { creatorID } = req.params;
     const { name, role, sagaComic, otherComics } = req.body;
-    const { success, message, status, result } = await CreatorService.updateCreator({ creatorID, name, role, sagaComic, otherComics });
+    const { result } = await CreatorService.updateCreator({ creatorID, name, role, sagaComic, otherComics });
     res.status(status).json({ code: status, success, message, data: { result } });
   }
 
@@ -33,8 +46,12 @@ class CreatorController {
     req: Request,
     res: Response
   ) {
+    const success: boolean = true;
+    const message: string = 'Creator deleted';
+    const status: number = StatusCodes.OK;
+
     const { creatorID } = req.params;
-    const { success, message, status, result } = await CreatorService.deleteCreator(creatorID);
+    const { result } = await CreatorService.deleteCreator(creatorID);
 
     res.status(status).json({ code: status, success, message, data: { result } });
   }
