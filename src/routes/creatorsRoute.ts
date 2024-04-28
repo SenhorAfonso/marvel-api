4b9a5701ad2ba5a9876387576e8c79a1e41685b0
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-28 14:37:01 -0300

    :sparkles: feat(creator): add getByCollectionSize method to creators route

--- src/routes/creatorsRoute.ts
+++ src/routes/creatorsRoute.ts
@@ -6,6 +6,7 @@ const creatorRouter = Router();
 creatorRouter.get('/fetch-creators', CreatorController.fetchCreators);
 creatorRouter.get('/reset-creators', CreatorController.resetCreators);
 creatorRouter.get('/creators', CreatorController.getCreators);
+creatorRouter.get('/creators/collectionSize', CreatorController.getByCollectionSize);
 creatorRouter.post('/creator', CreatorController.addCreator);
 creatorRouter.get('/creator/:creatorID', CreatorController.getSingleCreator);
 creatorRouter.put('/creator/:creatorID', CreatorController.updateCreator);
