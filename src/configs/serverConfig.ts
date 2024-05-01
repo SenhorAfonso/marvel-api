bdcd762f918be58629ad7ce0005a00c58db0e233
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-01 16:06:42 -0300

    :wrench: config(.env): add jwt and bcrypt secrets to .env

--- src/configs/serverConfig.ts
+++ src/configs/serverConfig.ts
@@ -8,7 +8,9 @@ const serverConfig = {
   MARVEL_API_AUTH: `?ts=${process.env.MARVEL_TS}&apikey=${process.env.MARVEL_PUBLIC_KEY}&hash=${process.env.MARVEL_HASH}`,
   IMAGE_QUALITY: process.env.IMAGE_QUALITY,
   IMAGE_EXTENSION: process.env.IMAGE_EXTENSION,
-  CACHE_EXPIRATION_TIME: process.env.CACHE_EXPIRATION_TIME
+  CACHE_EXPIRATION_TIME: process.env.CACHE_EXPIRATION_TIME,
+  JWT_SECRET: process.env.JWT_SECRET,
+  BCRYPT_SALT: process.env.BCRYPT_SALT
 };
 
 export default serverConfig;
