9850d1c5d758b1507fcc062af9031d909d6c8c8a
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-01 15:56:45 -0300

    :sparkles: feat(user-service): add token generation when user log in

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -1,6 +1,9 @@
+import jwt from 'jsonwebtoken';
 import IRegisterUserPayload from '../../interfaces/user/IRegisterUserPayload';
 import ILoginUserPayload from '../../interfaces/user/ILoginUserPayload';
 import userRepository from '../repositories/userRepository';
+import serverConfig from '../../configs/serverConfig';
+import IUserDocument from '../../interfaces/user/IUserDocument';
 
 class userService {
 
@@ -9,9 +12,20 @@ class userService {
     return result;
   }
 
-  static async loginUser(loginUserPayload: ILoginUserPayload) {
-    const result = await userRepository.loginUser(loginUserPayload);
-    return result;
+  static async loginUser(
+    loginUserPayload: ILoginUserPayload
+  ): Promise<{
+    result: {
+      user: IUserDocument,
+      token: string
+    }
+  }> {
+    const { result } = await userRepository.loginUser(loginUserPayload);
+
+    const userID = result.id;
+    const token = jwt.sign({ userID }, serverConfig.JWT_SECRET!);
+
+    return { result: { user: result, token } };
   }
 
 }
