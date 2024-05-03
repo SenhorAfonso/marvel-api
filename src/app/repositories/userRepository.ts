797aa93e3f23859550318586c1ded66bb13d474e
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-03 11:26:32 -0300

    :sparkles: feat(user-repository): prevent duplicated email from being registered

--- src/app/repositories/userRepository.ts
+++ src/app/repositories/userRepository.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import IRegisterUserPayload from '../../interfaces/user/IRegisterUserPayload';
 import ILoginUserPayload from '../../interfaces/user/ILoginUserPayload';
 import userModel from '../models/userModel';
@@ -5,10 +6,24 @@ import InternalServerError from '../errors/internalServerError';
 import APIUtils from '../utils/APIUtils';
 import BadRequestdError from '../errors/badRequestError';
 import IUserDocument from '../../interfaces/user/IUserDocument';
+import DuplicatedValueError from '../errors/duplicatedValueError';
 
 class userRepository {
 
   static async registerNewUser(registerUserPayload: IRegisterUserPayload) {
+    const { email } = registerUserPayload;
+    let emailAlreadyInUse: mongoose.Document | null;
+
+    try {
+      emailAlreadyInUse = await userModel.findOne({ email });
+    } catch (error) {
+      throw new InternalServerError();
+    }
+
+    if (emailAlreadyInUse) {
+      throw new DuplicatedValueError('The email is already in use');
+    }
+
     const result = await userModel.create(registerUserPayload);
     return { result };
   }
