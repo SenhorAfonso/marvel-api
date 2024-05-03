2f96ee6cbec1b9e0b6d2e58d65f5b4c774abdaf1
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-03 11:31:46 -0300

    :sparkles: feat(user-validation/routes): add validation middleware to user routes

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import userController from '../app/controllers/userControllers';
+import validationMiddleware from '../app/middlewares/validationMiddleware';
+import ValidateUser from '../app/validations/user/validateUser';
 
 const userRouter = Router();
 
-userRouter.post('/user/signup', userController.registerNewUser);
-userRouter.post('/user/login', userController.loginUser);
+userRouter.post('/user/signup', [
+  validationMiddleware('body', ValidateUser.registerUser())
+], userController.registerNewUser);
+
+userRouter.post('/user/login', [
+  validationMiddleware('body', ValidateUser.loginUser())
+], userController.loginUser);
 
 export default userRouter;
