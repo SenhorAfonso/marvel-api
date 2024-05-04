77f10ec461ae011e07441c10200017594c3150b2
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-04 19:05:23 -0300

    :recycle: refactor(apiUtils): make pagination parameter optional

--- src/app/utils/APIUtils.ts
+++ src/app/utils/APIUtils.ts
@@ -3,12 +3,12 @@ import IPagination from '../../interfaces/IPagination';
 
 class APIUtils {
 
-  static createQueryObject(pagination: IPagination) {
+  static createQueryObject(pagination?: IPagination) {
     const queryObject = {
-      limit: Number(pagination.limit) || 3,
-      skip: Number(pagination.skip) || 0,
-      sort: pagination.sort || 'asc',
-      page: Number(pagination.page) || 0
+      limit: Number(pagination?.limit) || 3,
+      skip: Number(pagination?.skip) || 0,
+      sort: pagination?.sort || 'asc',
+      page: Number(pagination?.page) || 0
     };
 
     return queryObject;
