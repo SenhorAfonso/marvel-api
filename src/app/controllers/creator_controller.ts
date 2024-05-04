63df62b428269445a08b34cb8cc5c368c6d9e312
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-04 18:59:08 -0300

    :recycle: refactor(creator-controller): remove comments

--- src/app/controllers/creator_controller.ts
+++ src/app/controllers/creator_controller.ts
@@ -4,35 +4,6 @@ import CreatorService from '../services/creator_service';
 
 class CreatorController {
   static async fetchCreators(req: Request, res: Response) {
-    /*
-     *#swagger.tags = ['Creators']
-     *#swagger.description = 'Endpoint to fetch creators from API.'
-     *#swagger.security = [{JWT: []}]
-     *#swagger.responses[201] = {
-     *description: 'Creators successfully fetched from API!',
-     *content: {
-     *"application/json": {
-     *schema: {
-     *  "$ref": "#/components/schemas/Creator"
-     *}
-     *}
-     *}
-     *}
-     *#swagger.responses[400] = {
-     *  description: 'Error fetching creators from API!'
-     *}
-     * #swagger.responses[401] = {
-     * description: 'Unauthorized'
-     * }
-     * #swagger.responses[404] = {
-     * description: 'Not Found'
-     * }
-     * #swagger.responses[500] = {
-     * description: 'Server Error'
-     * }
-     *
-     */
-
     const success: boolean = true;
     const message: string = 'Creators successfully fetched from API!';
     const status: number = StatusCodes.OK;
@@ -43,32 +14,6 @@ class CreatorController {
   }
 
   static async getCreators(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to fetch creators from API.'
-     * #swagger.responses[200] = {
-     *   description: 'All creators were retrieved!',
-     *   content: {
-     *     "application/json": {
-     *       schema: {
-     *         $ref: "#/components/schemas/Creator"
-     *       }
-     *     }
-     *   }
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Error fetching creators from API!'
-     * }
-     * #swagger.responses[401] = {
-     *   description: 'Unauthorized'
-     * }
-     * #swagger.responses[404] = {
-     *   description: 'Not Found'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'All creators were retrieved!';
     const status: number = StatusCodes.OK;
@@ -78,35 +23,6 @@ class CreatorController {
   }
 
   static async getSingleCreator(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to fetch a single creator by ID.'
-     * #swagger.parameters['creatorID'] = {
-     *   in: 'path',
-     *   description: 'ID of the creator to retrieve',
-     *   required: true,
-     *   type: 'string'
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'Single creator retrieved!',
-     *   content: {
-     *     "application/json": {
-     *       schema: {
-     *         $ref: "#/components/schemas/Creator"
-     *       }
-     *     }
-     *   }
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Error retrieving creator'
-     * }
-     * #swagger.responses[404] = {
-     *   description: 'Creator not found'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'Single creator retrieved!';
     const status: number = StatusCodes.OK;
@@ -117,32 +33,6 @@ class CreatorController {
   }
 
   static async addCreator(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to add a new creator.'
-     * #swagger.parameters['newCreator'] = {
-     *   in: 'body',
-     *   description: 'Creator object containing name, role, sagaComic, and otherComics',
-     *   required: true,
-     *   schema: { $ref: "#/components/schemas/CreatorInput" }
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'New creator added!',
-     *   content: {
-     *     "application/json": {
-     *       schema: {
-     *         $ref: "#/components/schemas/Creator"
-     *       }
-     *     }
-     *   }
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Error adding creator'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'New creator added!';
     const status: number = StatusCodes.OK;
@@ -153,41 +43,6 @@ class CreatorController {
   }
 
   static async updateCreator(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to update a creator by ID.'
-     * #swagger.parameters['creatorID'] = {
-     *   in: 'path',
-     *   description: 'ID of the creator to update',
-     *   required: true,
-     *   type: 'string'
-     * }
-     * #swagger.parameters['updatedCreator'] = {
-     *   in: 'body',
-     *   description: 'Updated creator object containing name, role, sagaComic, and otherComics',
-     *   required: true,
-     *   schema: { $ref: "#/components/schemas/CreatorInput" }
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'The creator was updated!',
-     *   content: {
-     *     "application/json": {
-     *       schema: {
-     *         $ref: "#/components/schemas/Creator"
-     *       }
-     *     }
-     *   }
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Error updating creator'
-     * }
-     * #swagger.responses[404] = {
-     *   description: 'Creator not found'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'The creator were updated!';
     const status: number = StatusCodes.OK;
@@ -199,28 +54,6 @@ class CreatorController {
   }
 
   static async deleteCreator(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to delete a creator by ID.'
-     * #swagger.parameters['creatorID'] = {
-     *   in: 'path',
-     *   description: 'ID of the creator to delete',
-     *   required: true,
-     *   type: 'string'
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'Creator deleted!',
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Error deleting creator'
-     * }
-     * #swagger.responses[404] = {
-     *   description: 'Creator not found'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'Creator deleted';
     const status: number = StatusCodes.OK;
@@ -232,16 +65,6 @@ class CreatorController {
   }
 
   static async resetCreators(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to reset creators.'
-     * #swagger.responses[200] = {
-     *   description: 'The creators were reset!'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'The creators were reseted!';
     const status: number = StatusCodes.OK;
@@ -253,25 +76,6 @@ class CreatorController {
   }
 
   static async getByCollectionSize(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to fetch creators by collection size.'
-     * #swagger.parameters['collSize'] = {
-     *   in: 'query',
-     *   description: 'Minimum size of the collection',
-     *   required: true,
-     *   type: 'integer'
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'Creators with collection size greater than specified were retrieved!'
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Bad Request'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const { collSize } = req.query;
     const success: boolean = true;
     const message: string = `Creators with collection greater than ${collSize} comics were retrieved!`;
@@ -282,25 +86,6 @@ class CreatorController {
   }
 
   static async getByNameLength(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['Creators']
-     * #swagger.description = 'Endpoint to fetch creators by name length.'
-     * #swagger.parameters['nameLength'] = {
-     *   in: 'query',
-     *   description: 'Minimum length of the creator name',
-     *   required: true,
-     *   type: 'integer'
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'Creators with name length greater than specified were retrieved!'
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Bad Request'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const { nameLength } = req.query;
     const success: boolean = true;
     const message: string = `Creators with name length greater than ${nameLength} characters were retrieved!`;
