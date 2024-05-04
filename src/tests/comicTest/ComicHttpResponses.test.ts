e8ba6ff86121f95e059df669f6cd36685ece51d8
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-04 19:11:55 -0300

    :recycle: refactor(comic-test): fix eslint

--- src/tests/comicTest/ComicHttpResponses.test.ts
+++ src/tests/comicTest/ComicHttpResponses.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
@@ -33,45 +32,44 @@ describe('Check for Comic Entity\'s routes', () => {
   describe('Fetch Comics route should', () => {
     it('be working', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/fetch-comics')
         .auth(token, { type: 'bearer' });
-  
+
       expect(response.body.code).toBe(StatusCodes.CREATED);
       expect(response.body.success).toBeTruthy();
       expect(response.body.message).toBe('Comics successfully fetched from API!');
       expect(response.body.data.available).toBe(20);
     }, 10000);
 
-
     it('return 500 error when database is not connected', async () => {
       await mongoose.connection.close();
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/fetch-comics')
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(500);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('An unknown error occured. Please try again later');
-    });
-  })
+    }, 10000);
+  });
 
   describe('Get All Comics route should', () => {
     it('be working', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       await request(server)
         .get('/api/v1/fetch-comics')
         .auth(token, { type: 'bearer' });
-  
+
       const response = await request(server)
         .get('/api/v1/comics')
         .auth(token, { type: 'bearer' });
-  
+
       expect(response.body.code).toBe(StatusCodes.OK);
       expect(response.body.success).toBeTruthy();
       expect(response.body.message).toBe('All comics were retrieved!');
@@ -80,21 +78,21 @@ describe('Check for Comic Entity\'s routes', () => {
 
     it('return 404 error when there is no comic registered', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/comics')
         .auth(token, { type: 'bearer' });
-  
+
       expect(response.body.code).toBe(404);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('There is no comic registered');
     });
-  })
+  });
 
   describe('Add Comic Route should', () => {
     it('be working', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const newComic = {
         title: 'How to make good tests with jest',
         description: 'A comic about tests',
@@ -102,13 +100,13 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const response = await request(server)
         .post('/api/v1/comic')
         .send(newComic)
         .auth(token, { type: 'bearer' });
-  
-      expect(response.body.code).toBe(200);
+
+      expect(response.body.code).toBe(201);
       expect(response.body.success).toBeTruthy();
       expect(response.body.message).toBe('New Comic added!');
     }, 15000);
@@ -116,7 +114,7 @@ describe('Check for Comic Entity\'s routes', () => {
     it('return 500 error when database is not connected', async () => {
       await mongoose.connection.close();
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const newComic = {
         title: 'How to make good tests with jest',
         description: 'A comic about tests',
@@ -124,23 +122,23 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const response = await request(server)
         .post('/api/v1/comic')
         .send(newComic)
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(500);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('An unknown error occured. Please try again later');
     });
-  })
+  });
 
   describe('Get Single Comic route should', () => {
     it('be working', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const newComic = {
         title: 'How to make good tests with jest',
         description: 'A comic about tests',
@@ -148,18 +146,18 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const ComicResponse = await request(server)
         .post('/api/v1/comic')
         .send(newComic)
         .auth(token, { type: 'bearer' });
-  
+
       const ComicID = ComicResponse.body.data.result._id;
-  
+
       const response = await request(server)
         .get(`/api/v1/comic/${ComicID}`)
         .auth(token, { type: 'bearer' });;
-  
+
       expect(response.body.code).toBe(200);
       expect(response.body.success).toBeTruthy();
       expect(response.body.message).toBe('Single comic retrieved!');
@@ -168,48 +166,48 @@ describe('Check for Comic Entity\'s routes', () => {
 
     it('return 400 error when the id format is invalid', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/comic/invalid')
         .auth(token, { type: 'bearer' });
-  
+
       expect(response.body.code).toBe(400);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('Id format is invalid');
     });
-  
+
     it('return 500 error when the database is not connected', async () => {
       await mongoose.connection.close();
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/comic/66329e3161d5caedd247e116')
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(500);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('An unknown error occured. Please try again later');
     });
-  
+
     it('return 404 error when the id is not associated with an record', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/comic/66329e3161d5caedd247e116')
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(404);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('there is no register asociated to this id');
     });
-  })
+  });
 
   describe('Update Comic Route should', () => {
     it('be working', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const newComic = {
         title: 'How to make good tests',
         description: 'A comic about tests',
@@ -217,14 +215,14 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const ComicResponse = await request(server)
         .post('/api/v1/comic')
         .send(newComic)
         .auth(token, { type: 'bearer' });
-  
+
       const ComicID = ComicResponse.body.data.result._id;
-  
+
       const newComicInfo = {
         title: 'How to make good tests with pytest',
         description: 'Another comic about tests, but with python',
@@ -232,12 +230,12 @@ describe('Check for Comic Entity\'s routes', () => {
         folder: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAfbG2qFSS5c9d7PKn3OjRC6CE6vJzlwyyI5vl75GN2g&s',
         pageCount: 35
       };
-  
+
       const response = await request(server)
         .put(`/api/v1/comic/${ComicID}`)
         .send(newComicInfo)
         .auth(token, { type: 'bearer' });;
-  
+
       expect(response.body.code).toBe(200);
       expect(response.body.success).toBeTruthy();
       expect(response.body.message).toBe('The Comic were updated!');
@@ -246,7 +244,7 @@ describe('Check for Comic Entity\'s routes', () => {
 
     it('return 404 error when the id is not associated with an record', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const updateComic = {
         title: 'How to make good tests with jest',
         description: 'A comic about tests',
@@ -254,12 +252,12 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const response = await request(server)
         .put('/api/v1/comic/66329e3161d5caedd247e116')
         .send(updateComic)
         .auth(token, { type: 'bearer' });
-  
+
       expect(response.body.code).toBe(404);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('there is no register asociated to this id');
@@ -267,7 +265,7 @@ describe('Check for Comic Entity\'s routes', () => {
 
     it('return 400 error when the id format is invalid', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const updateComic = {
         title: 'How to make good tests with jest',
         description: 'A comic about tests',
@@ -275,21 +273,21 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const response = await request(server)
         .put('/api/v1/comic/invalid')
         .send(updateComic)
         .auth(token, { type: 'bearer' });
-  
+
       expect(response.body.code).toBe(400);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('Id format is invalid');
     });
-  
+
     it('return 500 error when the database is not connected', async () => {
       await mongoose.connection.close();
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const updateComic = {
         title: 'How to make good tests with jest',
         description: 'A comic about tests',
@@ -297,23 +295,23 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const response = await request(server)
         .put('/api/v1/comic/66329e3161d5caedd247e116')
         .send(updateComic)
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(500);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('An unknown error occured. Please try again later');
     });
-  })
+  });
 
   describe('Delete Single Comic route should', () => {
     it('Delete Single Comic route should be working', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const newComic = {
         title: 'How to make good tests',
         description: 'A comic about tests',
@@ -321,18 +319,18 @@ describe('Check for Comic Entity\'s routes', () => {
         pageCount: 35,
         folder: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'
       };
-  
+
       const ComicResponse = await request(server)
         .post('/api/v1/comic')
         .send(newComic)
         .auth(token, { type: 'bearer' });
-  
+
       const ComicID = ComicResponse.body.data.result._id;
-  
+
       const response = await request(server)
         .delete(`/api/v1/comic/${ComicID}`)
         .auth(token, { type: 'bearer' });;
-  
+
       expect(response.body.code).toBe(200);
       expect(response.body.success).toBeTruthy();
       expect(response.body.message).toBe('Comic successfully deleted!');
@@ -342,26 +340,26 @@ describe('Check for Comic Entity\'s routes', () => {
     it('Delete comic route should return 500 error when the database is not connected', async () => {
       await mongoose.connection.close();
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .delete('/api/v1/comic/66329e3161d5caedd247e116')
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(500);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('An unknown error occured. Please try again later');
     });
-  })
+  });
 
   describe('Extra Comic Routes', () => {
     it('Reset Comic route should be working', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/reset-comics')
         .auth(token, { type: 'bearer' });
-  
+
       expect(response.body.code).toBe(200);
       expect(response.body.success).toBeTruthy();
       expect(response.body.message).toBe('Comics successfully reseted!');
@@ -371,49 +369,49 @@ describe('Check for Comic Entity\'s routes', () => {
     it('Reset comic route should return 500 error when the database is not connected', async () => {
       await mongoose.connection.close();
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/reset-comics')
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(500);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('An unknown error occured. Please try again later');
     });
-  
+
     it('Get by page count comic route should return 500 error when the database is not connected', async () => {
       await mongoose.connection.close();
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const response = await request(server)
         .get('/api/v1/comics/pageCount')
         .query({ pageCount: 30 })
         .auth(token, { type: 'bearer' });
-  
+
       await mongoose.connect(mongoURI);
       expect(response.body.code).toBe(500);
       expect(response.body.success).toBeFalsy();
       expect(response.body.error.message).toBe('An unknown error occured. Please try again later');
     });
-  
+
     it('Should have a longer response time due the empty cache', async () => {
       const token: string = serverConfig.USER_TOKEN_TEST!;
-  
+
       const responseWithNoCache = await request(server)
         .get('/api/v1/fetch-comics')
-        .auth(token, { type: 'bearer' })
-  
+        .auth(token, { type: 'bearer' });
+
       const responseTimeWithNoCache: number = Number(responseWithNoCache.headers['x-response-time']);
-  
+
       const responseWithCache = await request(server)
         .get('/api/v1/fetch-comics')
-        .auth(token, { type: 'bearer' })
-      
+        .auth(token, { type: 'bearer' });
+
       const responseTimeWithCache: number =  Number(responseWithCache.headers['x-response-time']);
-  
-      expect(responseTimeWithNoCache).toBeGreaterThan(responseTimeWithCache)
-    })
-  })
+
+      expect(responseTimeWithNoCache).toBeGreaterThan(responseTimeWithCache);
+    });
+  });
 
 });
