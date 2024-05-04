48cd1d3a8b262ddd150cc19b001b5387ce4f3b6b
Pedro Afonso <sirafonso005896@outlook.com>
2024-05-04 18:59:25 -0300

    :recycle: refactor(user-controller): remove comments

--- src/app/controllers/userControllers.ts
+++ src/app/controllers/userControllers.ts
@@ -4,32 +4,6 @@ import userService from '../services/userService';
 
 class userController {
   static async registerNewUser(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['User']
-     * #swagger.description = 'Endpoint to register a new user.'
-     * #swagger.parameters['newUser'] = {
-     *   in: 'body',
-     *   description: 'User object containing username, email, password, and confirmPassword',
-     *   required: true,
-     *   schema: { $ref: "#/components/schemas/UserSignup" }
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'User successfully registered!',
-     *   content: {
-     *     "application/json": {
-     *       schema: {
-     *         $ref: "#/components/schemas/User"
-     *       }
-     *     }
-     *   }
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Error registering user'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'User successfully registered!';
     const status: number = StatusCodes.CREATED;
@@ -38,39 +12,10 @@ class userController {
 
     const { result } = await userService.registerNewUser({ username, email, password, confirmPassword });
 
-    res.status(status).json({ code: status, success, message, data: { result } });
+    res.status(status).json({ code: status, success, message, data: result });
   }
 
   static async loginUser(req: Request, res: Response) {
-    /*
-     * #swagger.tags = ['User']
-     * #swagger.description = 'Endpoint to log in a user.'
-     * #swagger.parameters['loginUser'] = {
-     *   in: 'body',
-     *   description: 'User object containing email and password',
-     *   required: true,
-     *  schema: { $ref: "#/components/schemas/UserLogin" }
-     * }
-     * #swagger.responses[200] = {
-     *   description: 'User successfully logged in!',
-     *   content: {
-     *     "application/json": {
-     *       schema: {
-     *         $ref: "#/components/schemas/User"
-     *       }
-     *     }
-     *   }
-     * }
-     * #swagger.responses[400] = {
-     *   description: 'Error logging in user'
-     * }
-     * #swagger.responses[401] = {
-     *   description: 'Unauthorized'
-     * }
-     * #swagger.responses[500] = {
-     *   description: 'Server Error'
-     * }
-     */
     const success: boolean = true;
     const message: string = 'User successfully logged in!';
     const status: number = StatusCodes.OK;
@@ -78,7 +23,7 @@ class userController {
     const { email, password } = req.body;
     const { result } = await userService.loginUser({ email, password });
 
-    res.status(status).json({ code: status, success, message, data: { result } });
+    res.status(status).json({ code: status, success, message, data: result });
   }
 }
 
